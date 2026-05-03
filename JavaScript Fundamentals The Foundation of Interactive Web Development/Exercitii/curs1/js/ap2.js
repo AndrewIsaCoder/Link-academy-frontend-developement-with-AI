3ee07d13875625ecfe41e68240175aefbf84280c
Stoian Andrei <stoian.andrei239@gmail.com>
2026-05-03 22:39:34 +0300

    feat: Implement button click counter and remove inline JavaScript from HTML

--- JavaScript Fundamentals The Foundation of Interactive Web Development/Exercitii/curs1/js/ap2.js
+++ JavaScript Fundamentals The Foundation of Interactive Web Development/Exercitii/curs1/js/ap2.js
@@ -1,1 +1,7 @@
-alert("Salut din fisier extern!");
+countbuttonclicked = 0;
+
+function buttonCountdown(){
+    countbuttonclicked = countbuttonclicked + 1;
+    document.getElementById("butonapasat").innerText = "Utilizatorul a apasat butonul Home de " + countbuttonclicked + " ori!";
+    console.log("Butonul a fost apasat de " + countbuttonclicked + " ori!")
+}
